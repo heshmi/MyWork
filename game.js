48477b81df6a6eca8ee7f9f46ccb52653063b176
heshmi <54900314+heshmi@users.noreply.github.com>
2019-10-03 11:05:13 -0400

    dgfdgfdg

--- game.js
+++ game.js
@@ -14,9 +14,101 @@ function guess_number(){
       alert("guess lower");
     }
     else{
-      alert("Are you stupid")
+      alert("Are you ok")
 
 	 }
   }
 }
 
+function test(){
+  alert("Hello");
+  console.log("Hi");
+
+
+}
+
+
+function test1(){
+  alert("You");
+  console.log("Hi");
+
+
+}
+
+
+function test2(){
+  alert("Are");
+  console.log("Hi");
+
+
+}
+
+
+function test3(){
+  alert("A");
+  console.log("Hi");
+
+
+}
+
+
+function test4(){
+  alert("Human");
+  console.log("Hi");
+
+
+}
+
+
+function test5(){
+  alert("That");
+  console.log("Hi");
+
+
+}
+
+
+
+function test6(){
+  alert("Likes");
+  console.log("Hi");
+
+
+}
+
+
+
+function test7(){
+  alert("To");
+  console.log("Hi");
+
+
+}
+
+
+
+function test8(){
+  alert("Look");
+  console.log("Hi");
+
+
+}
+
+
+
+
+function test9(){
+  alert("Like");
+  console.log("Hi");
+
+
+}
+
+
+
+function test10(){
+  alert("Humans");
+  console.log("Hi");
+
+
+}
